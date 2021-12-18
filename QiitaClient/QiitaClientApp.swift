0a91530e2b92a500d0211d83f7b801096b735764
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-12-18 15:28:19 +0900

    Initial Commit

---
+++ QiitaClient/QiitaClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaClientApp.swift
+//  QiitaClient
+//
+//  Created by Masato Ishikawa on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct QiitaClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
