c1b111bd39950038c01befce5f1b732925cd1a31
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-12-18 20:36:34 +0900

    Add Article model

---
+++ QiitaClient/Model/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  QiitaClient
+//
+//  Created by Masato Ishikawa on 2021/12/18.
+//
+
+struct Article: Decodable, Identifiable {
+    let id: String
+    let title: String
+    let url: String
+    let user: User
+}
+
+struct User: Decodable {
+    let name: String
+    let profileImageUrl: String
+}
