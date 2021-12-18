1afbc3347b88f5a4b914c4d8f9ed03fb1689026c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-12-18 23:59:03 +0900

    Add ArticlesViewModel

--- QiitaClient/ViewModel/ArticlesViewModel.swift
+++ QiitaClient/ViewModel/ArticlesViewModel.swift
@@ -1,8 +1,22 @@
-//
-//  ArticlesViewModel.swift
-//  QiitaClient
-//
-//  Created by Masato Ishikawa on 2021/12/18.
-//
-
 import Foundation
+
+class ArticlesViewModel: ObservableObject {
+    let BASE_URL = "https://qiita.com/api/v2/items?page=1&per_page=20&"
+    @Published var articles = [Article]()
+    
+    init() {
+        fetchArticles(query: "SwiftUI")
+    }
+    
+    func fetchArticles(query: String) {
+        guard let url = URL(string: BASE_URL + query) else { return }
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            do {
+                guard let data = data else { return }
+                self.articles = try JSONDecoder().decode([Article].self, from: data)
+            } catch {
+                print("error")
+            }
+        }
+    }
+}
